2338ac007ec77978dc05be6708f8f865942a477a
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-16 02:16:14 +0530

    Add increment decrement function

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,42 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import { getMetricInfo } from "./helpers";
 export default class AddEntry extends React.Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+
+  incremenet = (metric) => {
+    const { max, step } = getMetricInfo(metric);
+
+    this.setState((state) => {
+      const count = state[metric] + step;
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      };
+    });
+  };
+  decrement = (metric) => {
+    this.setState((state) => {
+      const count = state[metric] - getMetricInfo(metric).step;
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      };
+    });
+  };
+
+  slide = (metric, value) => {
+    this.setState(() => ({
+      [metric]: value,
+    }));
+  };
+
   render() {
     return (
       <View>
