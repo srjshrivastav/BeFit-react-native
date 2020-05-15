29f14df128c3311fec90cb8ba6a61de6dc883f36
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-16 01:54:00 +0530

    Add metic function in helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+import {
+  FontAwesome,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import { white } from "./colors";
+
 export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
     return true;
@@ -40,4 +49,80 @@ export function timeToString(time = Date.now()) {
     Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
   );
   return todayUTC.toISOString().split("T")[0];
+}
+
+export function getMatriceInfo(metric) {
+  const info = {
+    run: {
+      displayName: "Run",
+      max: 50,
+      unit: "miles",
+      step: 1,
+      type: "steppers",
+      getIcons() {
+        return (
+          <View>
+            <MaterialIcons name="directions-run" color={"black"} size={35} />
+          </View>
+        );
+      },
+    },
+    swim: {
+      displayName: "Swim",
+      max: 9900,
+      unit: "meters",
+      step: 100,
+      type: "steppers",
+      getIcons() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="swim" color={"black"} size={35} />
+          </View>
+        );
+      },
+    },
+    bike: {
+      displayName: "Bike",
+      max: 100,
+      unit: "miles",
+      step: 1,
+      type: "steppers",
+      getIcons() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="Bike" color={"black"} size={35} />
+          </View>
+        );
+      },
+    },
+    sleep: {
+      displayName: "Sleep",
+      max: 24,
+      unit: "hours",
+      step: 1,
+      type: "slider",
+      getIcons() {
+        return (
+          <View>
+            <FontAwesome name="bed" color={"black"} size={35} />
+          </View>
+        );
+      },
+    },
+    eat: {
+      displayName: "Eat",
+      max: 10,
+      unit: "rating",
+      step: 1,
+      type: "slider",
+      getIcons() {
+        return (
+          <View>
+            <MaterialCommunityIcons name="food" color={"black"} size={35} />
+          </View>
+        );
+      },
+    },
+  };
+  return metric === undefined ? info : info[metric];
 }
