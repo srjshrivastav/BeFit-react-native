ade97fb3baae96d1ea34cc3a8d3ec1169d77c11c
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-16 02:01:04 +0530

    Add New component

---
+++ components/AddEntry.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class AddEntry extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello In Entry</Text>
+      </View>
+    );
+  }
+}
