f30b51b3badf717e42f656662d64614f34860fcd
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-17 13:21:15 +0530

    Add store from react-redux

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 export default function App() {
+  const store = createStore(reducer);
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store={store}>
+      <View>
+        <AddEntry />
+      </View>
+    </Provider>
   );
 }
