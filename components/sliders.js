87eebfd54a59d6651dd3ca1a24ddf12c558ff190
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-18 01:47:19 +0530

    add Some More Styling

--- components/sliders.js
+++ components/sliders.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { View, Text, Slider, StyleSheet } from "react-native";
+import { gray } from "../utils/colors";
 
 export default function Sliders({ max, unit, step, value, onChange }) {
   return (
-    <View>
+    <View styles={styles.row}>
       <Slider
+        style={{ flex: 1 }}
         step={step}
         maximumValue={max}
         minimumValue={0}
         value={value}
         onValueChange={onChange}
       />
+      <View style={styles.metricCounter}>
+        <Text style={{ fontSize: 22, textAlign: "center" }}>{value}</Text>
+        <Text style={{ fontSize: 16, color: gray }}>{unit}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  row: {
+    flexDirection: "row",
     flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
     alignItems: "stretch",
+  },
+  metricCounter: {
+    width: 85,
     justifyContent: "center",
   },
 });
