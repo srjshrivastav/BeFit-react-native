25230f1f99f5e7a8818bc43d1ea78e3c04a24dae
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-20 20:24:54 +0530

    Styled Live Compenent

--- components/Live.js
+++ components/Live.js
@@ -7,12 +7,12 @@ import {
   StyleSheet,
 } from "react-native";
 import { Foundation } from "@expo/vector-icons";
-import { purple, white } from "../utils/colors";
+import { purple, white, lightPurp } from "../utils/colors";
 
 export default class Live extends React.Component {
   state = {
     coords: null,
-    status: "undetermined",
+    status: "go",
     direction: "",
   };
 
@@ -47,9 +47,21 @@ export default class Live extends React.Component {
     }
 
     return (
-      <View>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={styles.container}>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>North</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{400}Feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -77,5 +89,38 @@ const styles = StyleSheet.create({
   btnText: {
     color: white,
     fontSize: 20,
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center",
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center",
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple,
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255,255,255,0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5,
   },
 });
